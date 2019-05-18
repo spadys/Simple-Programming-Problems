187920723871feab9bf51c030806f515363dc01e
Lukas Venhoda <lukas.venhoda@outlook.cz>
2019-05-19 01:41:21 +0200

    RUST-Elementary-T4: Add test

--- Rust/1_Elementary/t4/src/main.rs
+++ Rust/1_Elementary/t4/src/main.rs
@@ -1,6 +1,27 @@
 use std::io;
 use std::str::FromStr;
 
+fn get_sum_of_all_numbers_from_one_to_n(n: u32) -> u32
+{
+    let mut sum : u32 = 0;
+
+    for i in 1..n+1
+    {
+        sum += i;
+    }
+
+    sum
+}
+
+#[test]
+fn test()
+{
+    assert_eq!(get_sum_of_all_numbers_from_one_to_n(1), 1);
+    assert_eq!(get_sum_of_all_numbers_from_one_to_n(2), 3);
+    assert_eq!(get_sum_of_all_numbers_from_one_to_n(10), 55);
+    assert_eq!(get_sum_of_all_numbers_from_one_to_n(20), 210);
+}
+
 fn main() -> io::Result<()>
 {
     println!("Please input a number. I will write out a sum of all numbers from 1 to n.");
@@ -17,12 +38,7 @@ fn main() -> io::Result<()>
         }
     };
 
-    let mut sum : u32 = 0;
-    for n in 1..number+1
-    {
-        sum += n;
-    }
-
+    let sum = get_sum_of_all_numbers_from_one_to_n(number);
     println!("Final sum of all numbers from 1 to {} (including) is {}", number, sum);
 
     Ok(())
